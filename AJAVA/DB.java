1aa4e061021fbdffc422a2b0a5d764e0c9931b29
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2019-11-05 23:55:19 +0530

    Update DB.java

--- AJAVA/DB.java
+++ AJAVA/DB.java
@@ -13,22 +13,22 @@ public class DB
     Connection con = DriverManager.getConnection("jdbc:odbc:emp");
     Statement stmt=con.createStatement();
  
-    //System.out.println("Before Insertion:");  
+    System.out.println("Before Insertion:");  
     ResultSet rs=stmt.executeQuery("select * from emp_details");  
     while(rs.next())  
      System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getInt(3));
-    /*
+    
     int id=106;
     String name="aman";
     int salary=40000;
-    stmt.executeUpdate("INSERT into emp_details(emp_id,emp_name,emp_salary) VALUES('"+id+"','"+name+"','"+salary+"')"); //insertion
+    stmt.executeUpdate("insert into emp_details(emp_id,emp_name,emp_salary) VALUES('"+id+"','"+name+"','"+salary+"')"); //insertion
     stmt.executeUpdate("update emp_details set emp_salary=100 where emp_id=100"); //updation 
     stmt.executeUpdate("delete from emp_details where emp_id=104");//deletion
     System.out.println("After Insertion");
     rs=stmt.executeQuery("select * from emp_details");
     while(rs.next())
      System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getInt(3));
-    */
+    
     con.close();  
     } catch (Exception e) {
       System.err.println("Exception: "+e.getMessage());
